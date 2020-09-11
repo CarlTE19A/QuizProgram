672fc8b9bd93932f9f1ab190e801c919ff115b51
Carl <nordholm.carl@gmail.com>
2020-09-11 03:01:04 +0200

    Questionwriter added

--- Queststions/Program.cs
+++ Queststions/Program.cs
@@ -23,16 +23,36 @@ namespace Queststions
 
                 int qMax = questions.Length;
                 int points = 0;
-            Questioner();
+                string intronum = "";
+                string quizName = "";
+                Boolean quizExists = true;
+                string overWrite = "";
+                Boolean newQ = true;
+                string[] currentTq = new string[100];
+                string[] currentTa1 = new string[100];
+                string[] currentTa2 = new string[100];
+                string[] currentTa3 = new string[100];
+                string[] currentTac = new string[100];
+                int currentG = 0;
+            Intro();
             
             void Intro(){
-                System.Console.WriteLine("This is Quiz Program");
-                System.Console.WriteLine("Press 1 to start the latest Quiz");
-                System.Console.WriteLine("Press 2 to make your own Quiz");
-                System.Console.WriteLine("Press 3 to load a Quiz [COMING SOON]");
+                intronum = ""; 
+                while(intronum != "1" && intronum != "2" && intronum != "3"){
+                    Console.Clear();
+                    Console.WriteLine("This is Quiz Program");
+                    Console.WriteLine("Press 1 to start the latest Quiz");
+                    Console.WriteLine("Press 2 to make your own Quiz [COMING SOON]");
+                    Console.WriteLine("Press 3 to load a Quiz [COMING SOON]");
+                    intronum = Console.ReadLine();
+                    }
+                if(intronum == "1"){Questioner();}
+                if(intronum == "2"){MakeQuiz();}
+                if(intronum == "3"){LoadQuiz();}
             }
 
             void Questioner(){
+                Console.Clear();
                 if(random==true)
                 {
 
@@ -56,13 +76,131 @@ namespace Queststions
                                     {
                                         points+=1;
                                     }
-                                currentQ++;
+                                
                             }
+                            currentQ++;
                         }
                     
                 }
-                System.Console.WriteLine("Congratulations you got " + points + " points");
-                Console.ReadLine();
+                if(points<=qMax/4){
+                    System.Console.WriteLine("You got " + points + " points, thats awful");
+                    Console.ReadLine();
+                }
+                else if(points>qMax/4 && points<=qMax/2){
+                    System.Console.WriteLine("You got " + points + " points, thats not good");
+                    Console.ReadLine();
+                }
+                else if(points>qMax/2 && points<qMax){
+                    System.Console.WriteLine("You got " + points + " points, thats good");
+                    Console.ReadLine();
+                }
+                else if(points == qMax){
+                    System.Console.WriteLine("You got " + points + " points, thats a perfect score");
+                    Console.ReadLine();
+                }
+                    else{
+                    System.Console.WriteLine("You got " + points + " points");
+                    Console.ReadLine();
+                }
+                
+            }
+        
+            void LoadQuiz()
+            {
+                intronum = "";
+                while(intronum != "1" && intronum != "2"){
+                    Console.Clear();
+                    Console.WriteLine("This is the site to load a Quiz");
+                    Console.WriteLine("To load you own quiz press 1");
+                    Console.WriteLine("To load the premade Quiz press 2");
+                    intronum = Console.ReadLine();
+                    
+                    
+                }
+                if(intronum == "1"){Console.Clear();}
+                if(intronum == "2")
+                    {
+                    Console.Clear();
+                    //Take Backups and overwrite Main .txts
+                    }
+            }
+
+            void MakeQuiz()
+            {
+                while(quizName == "" && quizExists == true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("So you want to make your own Quiz");
+                    System.Console.WriteLine("First you need to give it a name");
+                    quizName = Console.ReadLine();
+                    if (File.Exists(@quizName + "q.txt"))
+                    {
+                        overWrite = "";
+                        while(overWrite != "1" && overWrite != "2"){
+                            Console.Clear();
+                            Console.WriteLine("Sorry the file exists already");
+                            Console.WriteLine("Do you want to overwrite it?");
+                            Console.WriteLine("1 for yes, 2 for no");
+                            overWrite = Console.ReadLine();
+                            if(overWrite == "1")
+                            {
+                                File.Delete(@quizName + "q.txt");
+                                File.Delete(@quizName + "a1.txt");
+                                File.Delete(@quizName + "a2.txt");
+                                File.Delete(@quizName + "a3.txt");
+                                File.Delete(@quizName + "a4.txt");
+                            }
+                            if(overWrite == "2")
+                            {
+                                quizExists = true;
+                                quizName = "";
+                            }
+                        }
+                    }                
+                    
+                }
+                /*
+                File.Create(@quizName + "q.txt");
+                File.Create(@quizName + "a1.txt");
+                File.Create(@quizName + "a2.txt");
+                File.Create(@quizName + "a3.txt");
+                File.Create(@quizName + "ac.txt");
+                */
+                while(newQ == true)
+                {
+                    overWrite = "";
+                    System.Console.WriteLine("Write a Question");
+                    currentTq[currentG] = Console.ReadLine();
+                    System.Console.WriteLine("Write the first answer");
+                    currentTa1[currentG] = Console.ReadLine();
+                    System.Console.WriteLine("Write the second answer");
+                    currentTa2[currentG] = Console.ReadLine();
+                    System.Console.WriteLine("Write the third answer");
+                    currentTa3[currentG] = Console.ReadLine();
+                    System.Console.WriteLine("Write what is the correct answer [1, 2, 3]");
+                    currentTac[currentG] = Console.ReadLine();
+                    while(overWrite != "1" && overWrite != "2")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Do you want another question?");
+                        Console.WriteLine("1 for yes, 2 for no");
+                        overWrite = Console.ReadLine();
+                        if(overWrite == "1")
+                        {
+                            currentG++;
+                        }
+                        if(overWrite == "2")
+                        {
+                            newQ = false;
+                            File.WriteAllLines(@"text.txt", currentTq);
+                            File.WriteAllLines(@quizName + "q.txt", currentTq);
+                            File.WriteAllLines(@quizName + "a1.txt", currentTa1);
+                            File.WriteAllLines(@quizName + "a2.txt", currentTa2);
+                            File.WriteAllLines(@quizName + "a3.txt", currentTa3);
+                            File.WriteAllLines(@quizName + "ac.txt", currentTac);
+                        }
+                    }
+                }
             }
         }
     }
